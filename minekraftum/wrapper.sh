b7c13d0ebe8a6b7dbfeaf35d3e7d396fe1bc872f
K-A-R-I-M <karim.benothman@outlook.com>
2025-03-01 20:52:45 +0100

    [DEV] delete k8s deploy + add github actions

--- minekraftum/wrapper.sh
+++ minekraftum/wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 java -jar server.jar nogui
 sed -i "s/eula=false/eula=true/g" /app/eula.txt
-java -Xmx1024M -Xms1024M -jar server.jar nogui
+java -Xms 1024M -Xmx $RAM -jar server.jar nogui

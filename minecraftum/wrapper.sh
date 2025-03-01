4a8e40b96b64ccc835d844885a8c3af14f60861e
K-A-R-I-M <karim.benothman@outlook.com>
2025-03-02 00:47:31 +0100

    [DEV] update Dockerfile

--- minecraftum/wrapper.sh
+++ minecraftum/wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-java -jar server.jar nogui
-sed -i "s/eula=false/eula=true/g" /app/eula.txt
-java -Xms1G -Xmx$RAM -jar server.jar nogui
+java -jar ./server.jar nogui
+sed -i "s/eula=false/eula=true/g" ./eula.txt
+java -Xms1G -Xmx$RAM -jar ./server.jar nogui

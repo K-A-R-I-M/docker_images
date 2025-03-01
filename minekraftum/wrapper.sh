a024df1df8f017a982cfcd85e2eb5477e9e8cf0e
K-A-R-I-M <karim.benothman@outlook.com>
2025-03-01 21:15:12 +0100

    [DEV] improve wrapper

--- minekraftum/wrapper.sh
+++ minekraftum/wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 java -jar server.jar nogui
 sed -i "s/eula=false/eula=true/g" /app/eula.txt
-java -Xms 1024M -Xmx $RAM -jar server.jar nogui
+java -Xms 1G -Xmx $RAM -jar server.jar nogui
